63bf522d2cb9610dd9be6a3c9554ee96e70c9fd1
Igor Lialin <lis4991@gmail.com>
2025-01-19 11:34:15 +0500

    db connection created

--- app/db/dbConnection.go
+++ app/db/dbConnection.go
@@ -12,14 +12,7 @@ var (
 )
 
 func connectDb() {
-	psqlInfo := fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
-	)
+	psqlInfo := getPsqlInfo()
 
 	var err error
 	db, err = sql.Open("postgres", psqlInfo)
@@ -34,4 +27,17 @@ func connectDb() {
 	}
 
 	fmt.Println("db connected!")
+}
+
+func getPsqlInfo() string {
+	psqlInfo := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
+
+	return psqlInfo
 }
